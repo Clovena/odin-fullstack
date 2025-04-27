201dfdd37e8886f3aa4f8c385218889c681d2708
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-04-26 22:49:08 -0400

    Proc notes.

--- 01-ruby/04_advanced_ruby/02_blocks/lambdas.rb
+++ 01-ruby/04_advanced_ruby/02_blocks/lambdas.rb
@@ -30,3 +30,103 @@ my_age.(28) # rubocop:disable Style/LambdaCall
 my_age[28]
 my_age.=== 28 # rubocop:disable Style/CaseEquality
 # .call is best though.
+
+# Procs
+
+# A Proc is an object that can be used to store blocks
+# and pass them around like variables.
+# Technically, a lambda is a type of Proc object.
+
+my_proc = Proc.new { puts 'this is a proc' } # rubocop:disable Style/Proc
+my_proc.call
+# => this is a proc
+
+# Or, more succinctly:
+another_proc = proc { puts 'this is a proc too' }
+another_proc.call
+# => this is a proc too
+
+# Arguments are always declared inside || pipes
+bio = proc { |name, age| puts "#{name} is #{age} years old." }
+bio.call('Zac', 28)
+# => Zac is 28 years old.
+
+# Procs vs Lambdas
+
+# 1. Procs do not care about an exact number of arguments.
+# They will pass nil to any missing arguments.
+# Lambdas DO care about the exact number of arguments.
+# They will throw an error if the wrong number of args are given.
+
+# 2. When a lambda contains an explicit return,
+# it returns from the lambda block back to the caller.
+# A proc object returns from the context in which it is called.
+a_proc = proc.new { return }
+a_proc.call
+# => localJumpError (unexpected return)
+# If a proc returns inside a method, the method serves as
+# the context in which it was called.
+# Therefore, it returns from the method itself before any below code is executed.
+def my_method
+  a_proc = proc { return }
+  puts 'this line will be printed'
+  a_proc.call
+  puts 'this line is never reached'
+end
+my_method
+# => this line will be printed
+
+# Similarities do exist, though.
+
+# Both procs and lambdas support default arguments.
+my_proc = proc { |name = 'bob'| puts name }
+my_proc.call
+# => bob
+
+my_lambda = ->(name = 'r2d2') { puts name }
+my_lambda.call
+# => r2d2
+
+# Both procs and lambdas can be used as arguments to a method.
+def my_method(arg)
+  arg.call
+end
+
+my_lambda = -> { puts 'lambda' }
+my_proc = proc { puts 'proc' }
+
+my_method(my_lambda) # => lambda
+my_method(my_proc) # => proc
+
+# Capturing Blocks
+
+# Now, let's get to the good stuff.
+# Ruby allows for capturing blocks in a method definition
+# using a special argument denoted with &
+
+def method(&my_block)
+  my_block.call
+end
+method { puts 'Cool!' }
+# The block-capturing & parameter should always be last
+# in the case of multiple parameters.
+
+# Technically speaking, the & syntax provokes Ruby
+# to call a method called #to_proc on what's assigned to the variable.
+# In the above, & calls #to_proc on my_block, creating a proc object.
+# From there, #call can be used on the new proc object.
+
+arr = %w[1 2 3]
+arr.map(&:to_i)
+# Here, & calls #to_proc on the symbol :to_i,
+# which points to the method #to_i.
+
+# & works the other way as well:
+def method
+  # This method accepts ZERO arguments
+  yield
+end
+
+my_proc = proc { puts 'Here\'s my proc!' }
+method(my_proc) # => ERROR, expects ZERO arguments but gets one.
+method(&my_proc) # => Here's my proc!
