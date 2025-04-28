bf35e757aa6057a477ffc00233d424fdd0dae851
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-04-28 12:03:51 -0400

    Init recursion project within.

---
+++ 01-ruby/05_compsci/project_recursion/lib/recursion.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Recursive methods in TOP assignment
+module Recursion
+  def self.fibonacci(num)
+    puts num
+  end
+
+  def merge_sort(arr)
+    puts arr
+  end
+end
---
+++ 01-ruby/05_compsci/project_recursion/main.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require_relative 'lib/recursion'
+
+Recursion.fibonacci(5)
+Recursion.merge_sort([1, 2, 3, 4, 5])
