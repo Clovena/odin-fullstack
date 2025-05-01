bdf2327c68dba0a9a3c1a14fe8031ba15b38e80a
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-05-01 13:45:06 -0400

    Declare constant variables in Hashmap class and use in initialize method. Refactor Hashmap#to_s, Hashmap#hash, and declar #set and #at.

--- 01-ruby/05_compsci/04_hashmaps/lib/hashmap.rb
+++ 01-ruby/05_compsci/04_hashmaps/lib/hashmap.rb
@@ -5,23 +5,63 @@ require_relative 'linked_list'
 
 # Hashmap class
 class Hashmap
+  INITIAL_CAPACITY = 16
+  LOAD_FACTOR = 0.75
+  GOLDEN_RATIO = 1.618
+
   attr_accessor :capacity, :load_factor, :buckets
 
-  def initialize(capacity = 16, load_factor = 0.75)
-    @capacity = capacity
-    @load_factor = load_factor
-    @buckets = Array.new(@capacity)
+  ### High level methods
+  def initialize
+    @capacity = INITIAL_CAPACITY
+    @load_factor = LOAD_FACTOR
+    @buckets = Array.new(@capacity) { LinkedList.new }
   end
 
-  def fib_hash(key)
-    hash_code = 0
-    golden = 1.618
-    key.each_char do |char|
-      hash_code = (hash_code * golden).to_i + char.ord
+  def to_s
+    puts "Capacity: #{@capacity}"
+    @buckets.each_with_index do |bucket, index|
+      bucket.format(index)
     end
-    hash_code
   end
 
-  def set(key, value)
+  ### Methods to retrive metadata
+  def size
+    @buckets.reject(&:empty?).size
   end
+
+  def empty?
+    size.zero?
+  end
+
+  ### Methods to generate hashes, rehash, etc.
+  def hash_fibs(key)
+    hash_code = 0
+    key_str = "#{key}#{key.class}"
+    key_str.each_char do |char|
+      hash_code = (hash_code * GOLDEN_RATIO).to_i + char.ord
+    end
+    hash_code % @capacity
+  end
+
+  def rehash
+    # resize
+    @capacity *= 2
+  end
+
+  ### Methods to interact with hashmap elements
+  def set(key, value)
+    rehash if size >= (@capacity * @load_factor)
+    bucket = @buckets[hash_fibs(key)]
+    if bucket.contains? key
+      bucket.replace(key, value)
+    else
+      bucket.append(key, value)
+    end
+  end
+
+  def at(index)
+    @buckets.at(index)
+  end
+  alias [] at
 end
