b68fc6b3a748f2727e29a9337e63a4643d3d3138
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-05-15 16:33:52 -0400

    Add methods to traverse a move tree and print out path. Kinda cheated tho

--- 01-ruby/05_compsci/06_knights_travails/lib/knight.rb
+++ 01-ruby/05_compsci/06_knights_travails/lib/knight.rb
@@ -18,16 +18,35 @@ class Knight
     [2, -1]
   ].freeze
 
-  attr_accessor :loc, :history
+  attr_accessor :loc, :parent, :children
 
   def initialize(loc = [1, 1], parent = nil)
     @loc = loc
     @parent = parent
     @children = Board.available_squares(self)
-    # @history = []
   end
 
   def moves
     MOVEMENT
+  end
+
+  def move_tree(goal_coords)
+    # debugger
+    queue = []
+    current = self
+    move_hist = []
+    until current.loc == goal_coords
+      current.children.each do |child|
+        queue << Knight.new(child, current) unless move_hist.include?(current) || move_hist.include?(child)
+      end
+      move_hist << current
+      current = queue.shift
+    end
+    display_parent(current)
+  end
+
+  def display_parent(piece)
+    display_parent(piece.parent) unless piece.parent.nil?
+    p piece.loc
   end
 end
--- 01-ruby/05_compsci/06_knights_travails/main.rb
+++ 01-ruby/05_compsci/06_knights_travails/main.rb
@@ -6,12 +6,14 @@ require_relative 'lib/move'
 require_relative 'lib/knight'
 
 n = Knight.new([3, 3])
-puts "The knight is on #{Board.coords_to_algebraic(n.loc)}."
-puts "The last move was #{Board.coords_to_algebraic(n.loc, n)}."
+# puts "The knight is on #{Board.coords_to_algebraic(n.loc)}."
+# puts "The last move was #{Board.coords_to_algebraic(n.loc, n)}."
 
-puts Move.to(n, [4, 5])
+# puts Move.to(n, [4, 5])
 
-puts 'Now for the available squares:'
-Board.available_squares(n).each do |coords|
-  puts Board.coords_to_algebraic(coords)
-end
+# puts 'Now for the available squares:'
+# Board.available_squares(n).each do |coords|
+#   puts Board.coords_to_algebraic(coords)
+# end
+
+n.move_tree([8, 8])
