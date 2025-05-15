15125b9104e1a2085f6d40b0ec56f3c05c423ab5
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-05-15 15:54:42 -0400

    Write some code to test features in main.rb

--- 01-ruby/05_compsci/06_knights_travails/main.rb
+++ 01-ruby/05_compsci/06_knights_travails/main.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
+require 'pry-byebug'
+require_relative 'lib/board'
+require_relative 'lib/move'
 require_relative 'lib/knight'
 
-knight = Knight.new
-p knight.available_squares
+n = Knight.new([3, 3])
+puts "The knight is on #{Board.coords_to_algebraic(n.loc)}."
+puts "The last move was #{Board.coords_to_algebraic(n.loc, n)}."
+
+puts Move.to(n, [4, 5])
+
+puts 'Now for the available squares:'
+Board.available_squares(n).each do |coords|
+  puts Board.coords_to_algebraic(coords)
+end
