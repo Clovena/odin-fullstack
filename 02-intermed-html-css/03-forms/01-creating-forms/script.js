72ed89977a7ec93cc2d2c85df16c573ac0850de1
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-06-16 17:52:30 -0400

    WIP: notes on html forms.

---
+++ 02-intermed-html-css/03-forms/01-creating-forms/script.js
@@ -0,0 +1,7 @@
+const username = document.querySelector("#username")
+const greetMeButton = document.querySelector(".greet-btn")
+const greetingOutput = document.querySelector(".greeting")
+
+greetMeButton.addEventListener('click', (event) => {
+   greetingOutput.innerText = `Hello ${username.value}`;
+})
